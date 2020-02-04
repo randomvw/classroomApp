728ccb68627fe3a1aecce5e7e7315af6d7154974
Caleb <randomvw@gmail.com>
2020-02-04 17:48:25 -0600

    added process.env.port for heroku use

--- index.js
+++ index.js
@@ -52,8 +52,11 @@ app.use(routes);
 
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}!`));
+
+
+
 
 //! HOMEWORK
 // ADD phone number to this.
