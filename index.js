ef1a02747a6ddc17c68f3f81e8bb5c85b6a0fc87
Caleb <randomvw@gmail.com>
2020-02-04 18:31:10 -0600

    ugh

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ app.use(routes);
 
 
 
-const $PORT = process.env.PORT || 3000;
-app.listen($PORT, () => console.log(`Listening on port ${port}!`));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}!`));
 
 
 
