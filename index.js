de2d2fdb66b16707c58f4645020c3227d69da4ab
Caleb <randomvw@gmail.com>
2020-02-04 18:25:13 -0600

    changed port

--- index.js
+++ index.js
@@ -52,8 +52,8 @@ app.use(routes);
 
 
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}!`));
+const $PORT = process.env.PORT || 3000;
+app.listen($PORT, () => console.log(`Listening on port ${port}!`));
 
 
 
